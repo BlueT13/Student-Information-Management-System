89e6d819e49cd4d7478adf419aa38c5cdcc03389
DragonT13 <alex23456@naver.com>
2023-03-18 23:55:57 +0900

    솔루션 파일, 프로젝트 파일, .gitignore 파일 추가

---
+++ src/StudentInformationManagementSystem/MainClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentInformationManagementSystem
+{
+	internal class MainClass
+	{
+		public static void Main(string[] args)
+		{
+            Console.WriteLine("Hello World");
+        }
+	}
+}
